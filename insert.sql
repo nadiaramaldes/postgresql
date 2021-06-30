3fdc2372783181cbad89572b9b8d5b83b89bfe51
nadiaramaldes <86744174+nadiaramaldes@users.noreply.github.com>
2021-06-30 16:39:52 -0300

    Create insert.sql

---
+++ insert.sql
@@ -0,0 +1,17 @@
+INSERT INTO tb_diretor
+VALUES
+(1, 'Steven Spielberg'),
+(2, 'James Cameron'),
+(3, 'Quentin Tarantino');
+
+
+INSERT INTO tb_produtora (id_produtora, nome_produtora) VALUES (1,'20th Century Studios');
+INSERT INTO tb_produtora (id_produtora, nome_produtora) VALUES (2,'Sony Pictures');
+INSERT INTO tb_produtora (id_produtora, nome_produtora) VALUES (3,'Paramount Pictures');
+
+INSERT INTO tb_filme
+VALUES
+(1, 'Django Livre',3,2,'Faroeste/Ação'),
+(2, 'Avatar',2,1, 'Ficção Científica'),
+(3, 'O Resgate do Soldado Ryan',1,3,'Guerra');
+
